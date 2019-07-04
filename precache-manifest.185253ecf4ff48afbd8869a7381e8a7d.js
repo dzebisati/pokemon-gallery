5db78d6cfa6fd6270910bf5baba56604e755698a
Azam <Azam@blabla.com>
2019-07-04 17:58:34 +0300

    Updates

---
+++ precache-manifest.185253ecf4ff48afbd8869a7381e8a7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a59ef35aaeb293f3ef52593bf53d91",
+    "url": "./index.html"
+  },
+  {
+    "revision": "60d2b751350fcdfa5d6e",
+    "url": "./static/css/main.e5fd2669.chunk.css"
+  },
+  {
+    "revision": "f679fd4182c5b012196f",
+    "url": "./static/js/2.0949c122.chunk.js"
+  },
+  {
+    "revision": "60d2b751350fcdfa5d6e",
+    "url": "./static/js/main.2f32f675.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
